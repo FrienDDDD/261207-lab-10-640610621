e1645015a010611eca21f15033eea8c1538e8827
FrienDDDD <friendjunk@gmail.com>
2022-09-22 16:48:21 +0700

    update

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -4,4 +4,23 @@ export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    const rooms = readDB();
+    const Idx = rooms.findIndex((x) => x.roomId === roomId);
+    if (Idx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
+    if (
+      rooms[Idx].messages.findIndex((x) => x.messageId === messageId) === -1
+    ) {
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
+    }
+
+    rooms[Idx].messages.splice(
+      rooms[Idx].messages.findIndex((x) => x.messageId === messageId),
+      1
+    );
+    writeDB(rooms);
+    return res.json({ ok: true });
+  }
 }
